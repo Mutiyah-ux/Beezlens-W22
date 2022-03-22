8b98e7c6b92d04857579050b1ad5be0ad147371a
Mutiyah-ux <77885610+Mutiyah-ux@users.noreply.github.com>
2022-03-22 00:19:17 -0400

    repo name changed

--- script.js
+++ script.js
@@ -25,3 +25,32 @@ function nav(event) {
   $("ul.nav a").removeClass("active");
   $(this).addClass("active");
 }
+
+
+$("#menu_home").click(openHome);
+
+function openHome() {
+  $("#menu a").removeClass("active");
+  $(this).addClass("active");
+  $("#content_home").siblings().hide();
+  $("#content_home").show();
+}
+
+$("#menu_about").click(openAbout);
+
+function openAbout() {
+  $(this).addClass("active");
+  $("#content_about").siblings().hide();
+  $("#content_about").show();
+}
+
+$("#menu_contact").click(openContact);
+
+function openContact() {
+  $(this).addClass("active");
+  $("#content_contact").siblings().hide();
+  $("#content_contact").show();
+}
+
+
+$("#menu_home").click();
