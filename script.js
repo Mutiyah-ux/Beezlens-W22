178f1c102d618c59332a2b73bd9c917db9fc853d
Mutiyah-ux <77885610+Mutiyah-ux@users.noreply.github.com>
2022-04-23 04:32:51 -0400

    commit changes

--- script.js
+++ script.js
@@ -18,46 +18,5 @@ function start() {
 
 $(document).on("click", "ul.nav a", nav);
 
-function nav(event) {
-  event.preventDefault();
-  var href = $(this).attr("href");
-  $("#content").load(href);
-  $("ul.nav a").removeClass("active");
-  $(this).addClass("active");
-}
-
-
-$("#menu_home").click(openHome);
-
-function openHome() {
-  $("#menu a").removeClass("active");
-  $(this).addClass("active");
-  $("#content_home").siblings().hide();
-  $("#content_home").show();
-}
-
-$("#menu_about").click(openAbout);
-
-function openAbout() {
-  $(this).addClass("active");
-  $("#content_about").siblings().hide();
-  $("#content_about").show();
-}
-
-$("#menu_join").click(openJoin);
-
-function openJoin() {
-  $(this).addClass("active");
-  $("#content_join").siblings().hide();
-  $("#content_join").show();
-}
-
-$("#menu_Login").click(openLogin);
-
-function openLogin() {
-  $(this).addClass("active");
-  $("#content_login").siblings().hide();
-  $("#content_login").show();
-}
 
 $("#menu_home").click();
