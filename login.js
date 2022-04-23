fda4ae6fa011ac27cf239997f71a3abc43a7ccbe
Mutiyah-ux <77885610+Mutiyah-ux@users.noreply.github.com>
2022-04-23 04:20:07 -0400

    Updated about page

--- login.js
+++ login.js
@@ -21,34 +21,34 @@ document.addEventListener("DOMContentLoaded", () => {
   const createAccountForm = document.querySelector("#createAccount");
 
   document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-      e.preventDefault();
-      loginForm.classList.add("form--hidden");
-      createAccountForm.classList.remove("form--hidden");
+    e.preventDefault();
+    loginForm.classList.add("form--hidden");
+    createAccountForm.classList.remove("form--hidden");
   });
 
   document.querySelector("#linkLogin").addEventListener("click", e => {
-      e.preventDefault();
-      loginForm.classList.remove("form--hidden");
-      createAccountForm.classList.add("form--hidden");
+    e.preventDefault();
+    loginForm.classList.remove("form--hidden");
+    createAccountForm.classList.add("form--hidden");
   });
 
   loginForm.addEventListener("submit", e => {
-      e.preventDefault();
+    e.preventDefault();
 
-      // Perform your AJAX/Fetch login
+    // Perform your AJAX/Fetch login
 
-      setFormMessage(loginForm, "error", "Invalid username/password combination");
+    setFormMessage(loginForm, "error", "Invalid username/password combination");
   });
 
   document.querySelectorAll(".form__input").forEach(inputElement => {
-      inputElement.addEventListener("blur", e => {
-          if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
-              setInputError(inputElement, "Username must be at least 10 characters in length");
-          }
-      });
+    inputElement.addEventListener("blur", e => {
+      if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 10) {
+        setInputError(inputElement, "Username must be at least 10 characters in length");
+      }
+    });
 
-      inputElement.addEventListener("input", e => {
-          clearInputError(inputElement);
-      });
+    inputElement.addEventListener("input", e => {
+      clearInputError(inputElement);
+    });
   });
 });
